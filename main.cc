b1949201bee82026d15a22304393cb2e206378f1
yamunan-bitset <yamunan.bitset@gmail.com>
2021-11-20 19:02:29 +0000

    3d movement

--- main.cc
+++ main.cc
@@ -11,6 +11,24 @@
 #include <ctime>
 
 float xrot, yrot, zrot, radius;
+float angle = 0.0f;
+float lx = 0.0f,lz = -1.0f;
+float x = 0.0f, z = 5.0f;
+float deltaAngle = 0.0f;
+float deltaMove = 0;
+
+void computePos(float deltaMove)
+{
+  x += deltaMove * lx * 0.1f;
+  z += deltaMove * lz * 0.1f;
+}
+
+void computeDir(float deltaAngle)
+{
+  angle += deltaAngle;
+  lx = sin(angle);
+  lz = -cos(angle);
+}
 
 void timer(int time)
 {
@@ -30,8 +48,14 @@ void init()
 
 void render()
 {
+  if (deltaMove)  computePos(deltaMove);
+  if (deltaAngle) computeDir(deltaAngle);
+
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glDepthMask(GL_DEPTH_TEST);
+  gluLookAt(    x, 1.0f, z,
+                x+lx, 1.0f,  z+lz,
+                0.0f, 1.0f,  0.0f);
   /* 2D rendering 
   glDisable(GL_DEPTH_TEST);
   glDisable(GL_LIGHTING);
@@ -113,6 +137,39 @@ void render()
   glutSwapBuffers();
 }
 
+void press(int key, int xx, int yy)
+{
+  switch (key)
+    {
+    case GLUT_KEY_LEFT  : deltaAngle = -0.01f; break;
+    case GLUT_KEY_RIGHT : deltaAngle = 0.01f;  break;
+    case GLUT_KEY_UP    : deltaMove  = 0.5f;   break;
+    case GLUT_KEY_DOWN  : deltaMove  = -0.5f;  break;
+    }
+}
+
+void release(int key, int x, int y)
+{
+  switch (key)
+    {
+    case GLUT_KEY_LEFT :
+    case GLUT_KEY_RIGHT: deltaAngle = 0.0f; break;
+    case GLUT_KEY_UP   :
+    case GLUT_KEY_DOWN : deltaMove  = 0.0f; break;
+    }
+}
+
+void resize(int w, int h)
+{
+  if (h == 0) h = 1;
+  float ratio =  w * 1.0 / h;
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glViewport(0, 0, w, h);
+  gluPerspective(45.0f, ratio, 0.1f, 100.0f);
+  glMatrixMode(GL_MODELVIEW);
+}
+
 int main(int argc, char** argv)
 {
   Display* dpy = XOpenDisplay(NULL);
@@ -125,7 +182,11 @@ int main(int argc, char** argv)
   init();
   glutCreateWindow("Planets");
   glutDisplayFunc(render);
+  glutIdleFunc(render);
   glutTimerFunc(0, timer, 0);
+  glutReshapeFunc(resize);
+  glutSpecialFunc(press);
+  glutSpecialUpFunc(release);
   glutMainLoop();
   return 0;
 }
